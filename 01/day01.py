565503f417afc2ad47a0b596b0894b4fa64b072a
Erik Bro Lavdal <erik@joshia.dk>
2024-10-30 10:51:31 +0100

    dag 1 opgave 2 løst

--- 01/day01.py
+++ 01/day01.py
@@ -17,9 +17,24 @@ def opgave1(input_data):
         
     print(f"svaret på opgave 1 er: {max(results)}")
     
+def opgave2(input_data):
+    results = []
+    tally = 0    
+    for line in input_data:
+        line = line.strip()
+        if line == "":
+            results.append(tally)
+            tally = 0
+        else:
+            tally += int(line)
+    results.append(tally)
+    results.sort()
+    print(f"svaret på opgave 2 er: {sum(results[-3:])}")
+    
     
 if __name__ == "__main__":
     with open("input.txt") as fin:
         lines = fin.readlines()
     
     opgave1(lines)
+    opgave2(lines)
