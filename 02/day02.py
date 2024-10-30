b1280505190f92ed1da622189d6027a2199e06a5
Erik Bro Lavdal <erik@joshia.dk>
2024-10-30 11:16:09 +0100

    dag 2 opgave 1 løst

---
+++ 02/day02.py
@@ -0,0 +1,43 @@
+"""
+Advent of Code besvarelser 2022 dag 2
+"""
+
+
+def opgave1(input_data):
+    total_score = 0
+    def get_outcome(line):
+        outcome_dict = {
+            "A X": "tie",
+            "A Y": "win",
+            "A Z": "loss",
+            "B X": "loss",
+            "B Y": "tie",
+            "B Z": "win",
+            "C X": "win",
+            "C Y": "loss",
+            "C Z": "tie",
+        }
+        return outcome_dict[line]
+        
+    for line in input_data:
+        outcome_score = {
+            "win": 6,
+            "tie": 3,
+            "loss": 0}
+        score_for_choosing = {"X": 1, "Y": 2, "Z": 3}
+        player1, player2 = line.strip().split(" ")
+        line_score = outcome_score[get_outcome(line.strip())] + score_for_choosing[player2]
+        total_score += line_score
+    
+    print(f"svaret på opgave 1 er: {total_score}")
+    
+def opgave2(input_data):
+    pass
+    
+    
+if __name__ == "__main__":
+    with open("input.txt") as fin:
+        lines = fin.readlines()
+    
+    opgave1(lines)
+    opgave2(lines)
