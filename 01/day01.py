3b51eec2a5f23344e35517712872d394aa6c362a
Erik Bro Lavdal <erik@joshia.dk>
2024-10-30 10:46:31 +0100

    Dag 1 opgave 1 løst

--- 01/day01.py
+++ 01/day01.py
@@ -2,3 +2,24 @@
 Advent of Code besvarelser 2022 dag 1
 """
 
+
+def opgave1(input_data):
+    results = []
+    tally = 0    
+    for line in input_data:
+        line = line.strip()
+        if line == "":
+            results.append(tally)
+            tally = 0
+        else:
+            tally += int(line)
+    results.append(tally)
+        
+    print(f"svaret på opgave 1 er: {max(results)}")
+    
+    
+if __name__ == "__main__":
+    with open("input.txt") as fin:
+        lines = fin.readlines()
+    
+    opgave1(lines)
