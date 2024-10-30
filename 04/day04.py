9f8df3b213e77514199ab2da75411cc65cc63f94
Erik Bro Lavdal <erik@joshia.dk>
2024-10-30 14:09:03 +0100

    dag 4 opgave 4 løst

--- 04/day04.py
+++ 04/day04.py
@@ -24,8 +24,15 @@ def opgave1(input_data):
     print(f"Opgave 1: {ans}")
     
 def opgave2(input_data):
-    pass
+    ans = 0
+    for line in input_data:
+        first, second = line.strip().split(",")
+        first_list = [x for x in range(int(first.split("-")[0]), int(first.split("-")[1])+1)]
+        second_list = [x for x in range(int(second.split("-")[0]), int(second.split("-")[1])+1)]
+        if any([x in second_list for x in first_list]):
+            ans +=1
     
+    print(f"Opgave 2: {ans}")
     
 if __name__ == "__main__":
     with open("input.txt") as fin:
